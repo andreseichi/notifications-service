6b045f30ce938a136a446ce4fd413b5706cc2254
andreseichi <andre_seichi@hotmail.com>
2022-12-14 20:18:55 -0300

    send notification

---
+++ src/application/use-cases/send-notification.spec.ts
@@ -0,0 +1,18 @@
+import { InMemoryNotificationsRepository } from '../../../test/reopositories/in-memory-notifications-repository';
+import { SendNotification } from './send-notification';
+
+describe('Send Notification', () => {
+  it('should create a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const sendNotification = new SendNotification(notificationsRepository);
+
+    const { notification } = await sendNotification.execute({
+      content: 'fake-content',
+      category: 'email',
+      recipientId: 'fake-recipient-id',
+    });
+
+    expect(notificationsRepository.notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications[0]).toEqual(notification);
+  });
+});
