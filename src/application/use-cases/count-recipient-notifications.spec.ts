f505c4118ffcb82e74016cde219eb51e880d5cac
andreseichi <andre_seichi@hotmail.com>
2022-12-15 11:17:11 -0300

    use-cases

---
+++ src/application/use-cases/count-recipient-notifications.spec.ts
@@ -0,0 +1,29 @@
+import { makeNotification } from '@test/factories/notification-factory';
+import { InMemoryNotificationsRepository } from '@test/reopositories/in-memory-notifications-repository';
+import { CountRecipientNotifications } from './count-recipient-notifications';
+
+describe('Count recipient notifications', () => {
+  it('should be able to count recipient notifications', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const countRecipientNotifications = new CountRecipientNotifications(
+      notificationsRepository,
+    );
+
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'recipient-id-1' }),
+    );
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'recipient-id-1' }),
+    );
+
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'recipient-id-2' }),
+    );
+
+    const { count } = await countRecipientNotifications.execute({
+      recipientId: 'recipient-id-1',
+    });
+
+    expect(count).toEqual(2);
+  });
+});
