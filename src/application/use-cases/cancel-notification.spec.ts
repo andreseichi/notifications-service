7fd77209c67d7dc8db637aa8b3a2a127057dd606
andreseichi <andre_seichi@hotmail.com>
2022-12-14 23:10:49 -0300

    cancel notification

---
+++ src/application/use-cases/cancel-notification.spec.ts
@@ -0,0 +1,39 @@
+import { Notification } from '@application/entities/notification';
+import { Content } from '@application/entities/notification-content';
+import { InMemoryNotificationsRepository } from '@test/reopositories/in-memory-notifications-repository';
+import { CancelNotification } from './cancel-notification';
+import { NotificationNotFound } from './errors/notification-not-found';
+
+describe('Cancel Notification', () => {
+  it('should create a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    const notification = new Notification({
+      category: 'email',
+      content: new Content('fake-content'),
+      recipientId: 'fake-recipient-id',
+    });
+
+    await notificationsRepository.create(notification);
+
+    await cancelNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationsRepository.notifications[0].cancelledAt).toEqual(
+      expect.any(Date),
+    );
+  });
+
+  it('should not be able to cancel a non existing notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'fake-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
