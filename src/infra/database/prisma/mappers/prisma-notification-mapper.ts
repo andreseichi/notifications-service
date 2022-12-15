d2cb7c6d26d55c8be7c49efbef39e761f3ea460b
andreseichi <andre_seichi@hotmail.com>
2022-12-15 11:28:59 -0300

    repository prisma

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,4 +1,6 @@
+import { Notification as RawNotification } from '@prisma/client';
 import { Notification } from '@application/entities/notification';
+import { Content } from '@application/entities/notification-content';
 
 export class PrismaNotificationMapper {
   static toPrisma(notification: Notification) {
@@ -10,5 +12,18 @@ export class PrismaNotificationMapper {
       readAt: notification.readAt,
       createdAt: notification.createdAt,
     };
+  }
+
+  static toDomain(raw: RawNotification): Notification {
+    return new Notification(
+      {
+        content: new Content(raw.content),
+        category: raw.category,
+        recipientId: raw.recipientId,
+        readAt: raw.readAt,
+        createdAt: raw.createdAt,
+      },
+      raw.id,
+    );
   }
 }
